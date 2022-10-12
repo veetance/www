8878ca2117dad5aeebb6db44843acded3d216ec6
divine.okonkwo <divine.okonkwo@uleth.ca>
2022-10-12 11:47:44 -0600

    added pages

--- js/index.js
+++ js/index.js
@@ -2,5 +2,28 @@ var app = new Framework7({
     // App root element
     el: '#app',
     // ... other parameters
+
+    routes: [
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      {
+        path: '/page2/',
+        url: 'pages/page2.html',
+      },
+    ],
+
+    routes: [
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      {
+        path: '/page3/',
+        url: 'pages/page3.html',
+      },
+    ],
+
   });
 var mainView = app.views.create('.view-main')
