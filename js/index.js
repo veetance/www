1fe9e8869ffc608d128da7b869981ac6d7c05cc5
divine.okonkwo <divine.okonkwo@uleth.ca>
2022-10-16 00:13:27 -0600

    3 Pages routed correctly

--- js/index.js
+++ js/index.js
@@ -2,5 +2,44 @@ var app = new Framework7({
     // App root element
     el: '#app',
     // ... other parameters
+
+    routes: [
+
+
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      {
+        path: '/page1/',
+        url: 'pages/page1.html',
+      },
+
+    
+
+
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      {
+        path: '/page2/',
+        url: 'pages/page2.html',
+      },
+
+      {
+        path: '/',
+        url: 'index.html',
+      },
+      {
+        path: '/page3/',
+        url: 'pages/page3.html',
+      },
+    ],
+
+   
+
+
+
   });
 var mainView = app.views.create('.view-main')
